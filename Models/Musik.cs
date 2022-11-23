2ca1a6a71358713a3a148a9d4f612d794d027b0a
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2022-11-23 20:48:26 +0700

    Create Musik.cs

---
+++ Models/Musik.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pert6_50420221.Models
+{
+    public class Musik
+    {
+        public int id { get; set; }
+        public string judul { get; set; }
+        public string penyanyi { get; set; }
+        public int tahun { get; set; }
+    }
+}
